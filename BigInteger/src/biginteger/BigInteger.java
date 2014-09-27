583c897a776d047df21e323ff383d0f07d7f9c01
matychp <matychp@gmail.com>
2014-09-26 21:46:16 -0300

    Eliminado el else del ciclo for del constructor con un String como parametro, motivo estaba demás

--- BigInteger/src/biginteger/BigInteger.java
+++ BigInteger/src/biginteger/BigInteger.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package biginteger;
 
 import java.util.LinkedList;
@@ -27,8 +22,6 @@ public class BigInteger {
             if (Character.isDigit(letra)) {
                 String aux = "" + letra;
                 bigInt.add(Integer.parseInt(aux));
-            } else {
-                //return throw new SeEncontroUnaLetra;
             }
         }
     }
