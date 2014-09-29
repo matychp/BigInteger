751e321269977c8fa00a0b9fd475d7c71cf4b9ed
matychp <matychp@gmail.com>
2014-09-29 01:55:52 -0300

    Clase Test mas ordenada

--- BigInteger/src/biginteger/Test.java
+++ BigInteger/src/biginteger/Test.java
@@ -11,35 +11,34 @@ public class Test {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         
-        BigInteger unBIconString = new BigInteger("0000123");
-        System.out.println("String: " + unBIconString.toString());
+        System.out.print("Ingrese un numero: ");
+        String unNumero = in.next();
+        BigInteger biString = new BigInteger(unNumero);
+        System.out.println("BigInteger String: " + biString.toString());
         
         int unArreglo[] = {1,2,3,4,5,6,7,8,9};
-        //int unArreglo[] = {0,0,0,0,0,0,0,0};
         BigInteger unBIconArreglo = new BigInteger(unArreglo);
-        System.out.println("Arreglo: " + unBIconArreglo.toString());
+        System.out.println("BigInteger Arreglo: " + unBIconArreglo.toString());        
         
-        /*
         System.out.print("Ingrese la cantidad de elementos del arreglo: ");
         int n = in.nextInt();
         int unArregloPorTeclado[] = new int[n];
         int i = 0;
         while(i < unArregloPorTeclado.length){
-            System.out.println("Ingrese el numero: ");
+            System.out.print("Ingrese el numero " + i + ": ");
             int num = in.nextInt();
             unArregloPorTeclado[i] = num;
             i++;
         }
         BigInteger unBIconArregloPorTeclado = new BigInteger(unArregloPorTeclado);
-        System.out.println(unBIconArregloPorTeclado.toString());
-        //*/
+        System.out.println("BigInteger Arreglo cargado por teclado: " + unBIconArregloPorTeclado.toString());        
         
-        BigInteger unBIA = new BigInteger("123");
-        BigInteger unBIB = new BigInteger("123");
-        System.out.println("Suma: " + unBIA.suma(unBIB));
-        
-        BigInteger unBIAA = new BigInteger("004456");
-        BigInteger unBIBB = new BigInteger("123456");
-        System.out.println("Son iguales?: " + unBIAA.equals(unBIBB));        
+        System.out.print("Ingrese un numero A a sumar: ");
+        String unNumeroA = in.next();
+        BigInteger biA = new BigInteger(unNumeroA);
+        System.out.print("Ingrese un numero B a sumar: ");
+        String unNumeroB = in.next();
+        BigInteger biB = new BigInteger(unNumeroB);
+        System.out.println("Suma: " + biA.suma(biB));
     }
 }
