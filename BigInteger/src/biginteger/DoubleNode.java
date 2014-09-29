4a0a5ebb411986ea88c76ddfaca82abc33526135
matychp <matychp@gmail.com>
2014-09-29 03:47:46 -0300

    Cambiado el nombre de la clase Node a DoubleNode, para poder diferenciar nodos con solo puntero next, de nodos dobles con punteros a next y previous.

---
+++ BigInteger/src/biginteger/DoubleNode.java
@@ -0,0 +1,55 @@
+package biginteger;
+
+public class DoubleNode < E extends Comparable >
+{
+   private E info;
+   private DoubleNode <E> next, previous;
+   
+   public DoubleNode ( )
+   {
+   }
+   
+   public DoubleNode (E e, DoubleNode <E> n, DoubleNode <E> p)
+   {
+     info = e;
+     next = n;
+     previous = p;
+   }
+   
+   public DoubleNode <E> getNext()
+   {
+     return next;
+   }
+   
+   public void setNext(DoubleNode <E> n)
+   {
+     next = n;
+   }
+   
+   public E getInfo()
+   {
+     return info;
+   }
+   
+   public void setInfo(E p)
+   {
+     info = p;
+   }
+   
+   public DoubleNode <E> getPrevious()
+   {
+       return previous;
+   }
+
+   public void setPrevious(DoubleNode <E> p)
+   {
+       previous = p;
+   }
+   
+   @Override
+   public String toString()
+   {
+     return info.toString();   
+   }
+}
+
